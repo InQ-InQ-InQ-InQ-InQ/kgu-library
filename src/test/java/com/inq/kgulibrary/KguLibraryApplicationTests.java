5429422fcc9eaed4a27f814f4f6b3f9d55bc0cb8
we4sley <galaxy138742@gmail.com>
2022-01-04 17:34:01 +0900

    chore: 프로젝트 초기셋팅

---
+++ src/test/java/com/inq/kgulibrary/KguLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inq.kgulibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KguLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
